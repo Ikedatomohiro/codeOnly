20696a5159360ce298c1a95d9b33067dd4ccc030
tomohiro <to.tomo.tomohiro@gmail.com>
2020-08-25 22:42:02 +0900

    Initial Commit

---
+++ codeOnly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  codeOnly
+//
+//  Created by 池田友宏 on 2020/08/25.
+//  Copyright © 2020 Tomohiro Ikeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
