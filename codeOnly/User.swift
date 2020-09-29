a258d5d9cc7224ad856ef6de57104497767a1153
kestencg <launchuponquest@gmail.com>
2020-09-29 21:05:36 +0900

    0929

---
+++ codeOnly/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  codeOnly
+//
+//  Created by 杉崎圭 on 2020/09/29.
+//  Copyright © 2020 Tomohiro Ikeda. All rights reserved.
+//
+
+import Foundation
+import FirebaseFirestore
+
+class Uselr {
+    static var usersRef = Firestore.firestore().collection("users")
+    
+    id
+    name
+}
